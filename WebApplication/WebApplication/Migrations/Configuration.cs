a9866a17a13e1c64e6c3351e1155ee0c3bb9d305
Mantas Antanaitis <manant1@ktu.lt>
2018-03-31 23:57:22 +0300

    DB fix

--- WebApplication/WebApplication/Migrations/Configuration.cs
+++ WebApplication/WebApplication/Migrations/Configuration.cs
@@ -5,14 +5,14 @@ namespace WebApplication.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<WebApplication.Models.ContainerCompetitors>
+    internal sealed class Configuration : DbMigrationsConfiguration<WebApplication.Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(WebApplication.Models.ContainerCompetitors context)
+        protected override void Seed(WebApplication.Models.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
